f50127f6e7f4782e8ad68d8e002a5a7e7a8cfbfc
Anjali-75 <anjali.gandhi@byjus.com>
2023-02-24 17:12:34 +0530

    frontend

--- app/src/main/java/com/example/legalsatta/Activities/MainActivity.kt
+++ app/src/main/java/com/example/legalsatta/Activities/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.legalsatta.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import com.example.legalsatta.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.splash_screen)
+
+        Handler().postDelayed({
+            val i = Intent(
+                this@MainActivity,
+                AuthActivity::class.java
+            )
+            startActivity(i)
+            finish()
+        }, 2000)
     }
 }
--- app/src/main/java/com/example/legalsatta/Fragments/LeaderBoard.kt
+++ app/src/main/java/com/example/legalsatta/Fragments/LeaderBoard.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.legalsatta.R
-
 
 
 class LeaderBoard : Fragment() {
@@ -16,6 +18,45 @@ class LeaderBoard : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leader_board, container, false)
+        var v = inflater.inflate(R.layout.fragment_leader_board, container, false)
+
+
+        var rankListView = v.findViewById<RecyclerView>(R.id.recycleViewLeaderboard)
+        rankListView .layoutManager = LinearLayoutManager(context)
+        rankListView .adapter = leaderboardAdapter(context, //list)
+
+            return v;
     }
+
+    class leaderboardAdapter(var context: Context, var leaderboardList: ArrayList<Leaderboard>) : RecyclerView.Adapter<leaderboardAdapter.rankViewHolder>(){
+
+        class rankViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+            var nameTextView = itemView.findViewById<TextView>(R.id.name)
+            var rank = itemView.findViewById<TextView>(R.id.rank)
+            var score = itemView.findViewById<TextView>(R.id.score)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): rankViewHolder {
+            val view = LayoutInflater.from(context). inflate(
+                R.layout.card_upcoming_matches, parent,
+                false
+            )
+            return rankViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return leaderboardList.size
+        }
+
+        override fun onBindViewHolder(holder: rankViewHolder, position: Int) {
+            var rankModel =  leaderboardList.get(position)
+            holder.nameTextView.text = rankModel.name
+            holder.rank.text = rankModel.rank.toString()
+            holder.score.text = rankModel.score.toString()
+        }
+    }
+
+
 }
+
+data class Leaderboard(val id: Int, val name: String, val rank: Int, val score: Int)
