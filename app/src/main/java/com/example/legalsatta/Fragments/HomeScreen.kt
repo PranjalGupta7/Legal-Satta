86b00c600fbbbd4b160d7617ae764948a2b8ee42
PranjalGupta7 <pranjal.gupta6@byjus.com>
2023-02-28 12:53:16 +0530

    fourth commit

--- app/src/main/java/com/example/legalsatta/Fragments/HomeScreen.kt
+++ app/src/main/java/com/example/legalsatta/Fragments/HomeScreen.kt
@@ -6,6 +6,7 @@ import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -25,8 +26,10 @@ class HomeScreen : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_home_screen, container, false)
-           val teamImg1 = v.findViewById<ImageView>(R.id.teamImg1)
-           val teamImg2 = v.findViewById<ImageView>(R.id.teamImg2)
+        val teamImg1 = v.findViewById<ImageView>(R.id.teamImg1)
+        val teamImg2 = v.findViewById<ImageView>(R.id.teamImg2)
+        val predictNowBtn = v.findViewById<TextView>(R.id.predictNowBtn)
+
         Glide
             .with(this)
             .load("https://i.pinimg.com/736x/70/15/05/701505a5bbe24320e3c33f26808cdaac.jpg")
@@ -40,6 +43,9 @@ class HomeScreen : Fragment() {
             .placeholder(R.drawable.ic_launcher_foreground)
             .into(teamImg2);
 
+        predictNowBtn.setOnClickListener {
+
+        }
 
         var timerTextView = v.findViewById<TextView>(R.id.timer)
 
