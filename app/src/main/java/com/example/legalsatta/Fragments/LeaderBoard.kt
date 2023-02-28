e10661e6cf47ffb7548971d100427128aac00629
PranjalGupta7 <pranjal.gupta6@byjus.com>
2023-02-28 16:54:16 +0530

    eighth commit

--- app/src/main/java/com/example/legalsatta/Fragments/LeaderBoard.kt
+++ app/src/main/java/com/example/legalsatta/Fragments/LeaderBoard.kt
@@ -28,36 +28,36 @@ class LeaderBoard : Fragment() {
 
         return v;
     }
-
-    class leaderboardAdapter(var context: Context, var leaderboardList: ArrayList<Leaderboard>) : RecyclerView.Adapter<leaderboardAdapter.rankViewHolder>(){
-
-        class rankViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-            var nameTextView = itemView.findViewById<TextView>(R.id.name)
-            var rank = itemView.findViewById<TextView>(R.id.rank)
-            var score = itemView.findViewById<TextView>(R.id.score)
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): rankViewHolder {
-            val view = LayoutInflater.from(context). inflate(
-                R.layout.rank_card, parent,
-                false
-            )
-            return rankViewHolder(view)
-        }
-
-        override fun getItemCount(): Int {
-            return leaderboardList.size
-        }
-
-        override fun onBindViewHolder(holder: rankViewHolder, position: Int) {
-            var rankModel =  leaderboardList.get(position)
-            holder.nameTextView.text = rankModel.name
-            holder.rank.text = rankModel.rank.toString()
-            holder.score.text = rankModel.score.toString()
-        }
-    }
-
-
 }
-
-data class Leaderboard(val id: Int, val name: String, val rank: Int, val score: Int)
+//    class leaderboardAdapter(var context: Context, var leaderboardList: ArrayList<Leaderboard>) : RecyclerView.Adapter<leaderboardAdapter.rankViewHolder>(){
+//
+//        class rankViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+//            var nameTextView = itemView.findViewById<TextView>(R.id.name)
+//            var rank = itemView.findViewById<TextView>(R.id.rank)
+//            var score = itemView.findViewById<TextView>(R.id.score)
+//        }
+//
+//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): rankViewHolder {
+//            val view = LayoutInflater.from(context). inflate(
+//                R.layout.rank_card, parent,
+//                false
+//            )
+//            return rankViewHolder(view)
+//        }
+//
+//        override fun getItemCount(): Int {
+//            return leaderboardList.size
+//        }
+//
+//        override fun onBindViewHolder(holder: rankViewHolder, position: Int) {
+//            var rankModel =  leaderboardList.get(position)
+//            holder.nameTextView.text = rankModel.name
+//            holder.rank.text = rankModel.rank.toString()
+//            holder.score.text = rankModel.score.toString()
+//        }
+//    }
+//
+//
+//}
+//
+//data class Leaderboard(val id: Int, val name: String, val rank: Int, val score: Int)
